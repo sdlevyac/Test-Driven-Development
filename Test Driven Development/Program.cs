676ada84f3fa5ac1b1437157154975486e355d15
Shaun Levy <sly@ndonline.ac.uk>
2024-06-24 08:53:53 +0100

    Added new functionality and unit tests

--- Test Driven Development/Program.cs
+++ Test Driven Development/Program.cs
@@ -18,9 +18,10 @@ namespace Test_Driven_Development
                 Console.WriteLine("Choose an option:");
                 Console.WriteLine("1. Search products based on product ID");
                 Console.WriteLine("2. Search suppliers based on supplier ID");
+                Console.WriteLine("3. Search customers based on supplier ID");
                 Console.WriteLine("0. Quit");
                 string input = Console.ReadLine();
-                if (new string[] { "0", "1", "2" }.Contains(input))
+                if (new string[] { "0", "1", "2", "3" }.Contains(input))
                 {
                     switch (input)
                     {
@@ -28,10 +29,13 @@ namespace Test_Driven_Development
                             running = false;
                             break;
                         case "1":
-                            GetProduct(ValidInt());
+                            ProductMenu();
                             break;
                         case "2":
-                            Console.WriteLine("supplier search not implemented");
+                            GetSupplier(ValidInt());
+                            break;
+                        case "3":
+                            CustomerMenu();
                             break;
                         default:
                             break;
@@ -52,7 +56,6 @@ namespace Test_Driven_Development
         }
         public static void GetProduct(int productID)
         {
-
             using var connection = new MySqlConnection(connStr);
             connection.Open();
             using var command = new MySqlCommand("SELECT productName FROM products WHERE productID = @productID;", connection);
@@ -62,8 +65,10 @@ namespace Test_Driven_Development
             {
                 Console.WriteLine(reader.GetString(0));
             }
-
-
+            else
+            {
+                Console.WriteLine($"no product found with id {productID}");
+            }
         }
         public static void GetSupplier(int supplierID)
         {            
@@ -76,6 +81,143 @@ namespace Test_Driven_Development
             {
                 Console.WriteLine(reader.GetString(0));
             }
+            else
+            {
+                Console.WriteLine($"no supplier found with id {supplierID}");
+            }
+        }
+        public static void ProductMenu()
+        {
+            bool running = true;
+            while (running)
+            {
+                Console.WriteLine("Searching products by product ID, Choose an option:");
+                Console.WriteLine("1. Get product name");
+                Console.WriteLine("2. Get category of product");
+                Console.WriteLine("3. Get supplier of product");
+                Console.WriteLine("0. Quit");
+                string input = Console.ReadLine();
+                if (new string[] { "0", "1", "2", "3" }.Contains(input))
+                {
+                    switch (input)
+                    {
+                        case "0":
+                            running = false;
+                            break;
+                        case "1":
+                            GetProduct(ValidInt());
+                            break;
+                        case "2":
+                            GetCategoryOfProduct(ValidInt());
+                            break;
+                        case "3":
+                            GetSupplierOfProduct(ValidInt());
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+        public static void GetCategoryOfProduct(int productID)
+        {
+            using var connection = new MySqlConnection(connStr);
+            connection.Open();
+            using var command = new MySqlCommand("SELECT categoryName FROM products, categories WHERE productID = @productID", connection);
+            command.Parameters.AddWithValue("@productID", productID);
+            using var reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                Console.WriteLine(reader.GetString(0));
+            }
+        }
+        public static void GetSupplierOfProduct(int productID)
+        {
+            using var connection = new MySqlConnection(connStr);
+            connection.Open();
+            using var command = new MySqlCommand("SELECT supplierName FROM products, suppliers WHERE productID = @productID " +
+                "AND products.supplierID = suppliers.supplierID;", connection);
+            command.Parameters.AddWithValue("@productID", productID);
+            using var reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                Console.WriteLine(reader.GetString(0));
+            }
+        }
+        public static void CustomerMenu()
+        {
+            bool running = true;
+            while (running)
+            {
+                Console.WriteLine("Searching customers by customer ID, Choose an option:");
+                Console.WriteLine("1. Get customer name");
+                Console.WriteLine("2. Get contact name (initals)");
+                Console.WriteLine("3. Get address, city, postcode, country");
+                Console.WriteLine("0. Quit");
+                string input = Console.ReadLine();
+                if (new string[] { "0", "1", "2", "3" }.Contains(input))
+                {
+                    switch (input)
+                    {
+                        case "0":
+                            running = false;
+                            break;
+                        case "1":
+                            GetCustomerName(ValidInt());
+                            break;
+                        case "2":
+                            GetContactName(ValidInt());
+                            break;
+                        case "3":
+                            GetFullCustomerAddress(ValidInt());
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+        public static void GetCustomerName(int customerID)
+        {
+            string customerName = "not found";
+            using var connection = new MySqlConnection(connStr);
+            connection.Open();
+            using var command = new MySqlCommand("SELECT customerName FROM customers WHERE customerID = @customerID;", connection);
+            command.Parameters.AddWithValue("@customerID", customerID);
+            using var reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                customerName = reader.GetString(0);
+            }
+            Console.WriteLine(customerName);
+        }
+        public static void GetContactName(int customerID)
+        {
+            string contactName = "not found";
+            using var connection = new MySqlConnection(connStr);
+            connection.Open();
+            using var command = new MySqlCommand("SELECT contactName FROM customers WHERE customerID = @customerID;", connection);
+            command.Parameters.AddWithValue("@customerID", customerID);
+            using var reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                contactName = reader.GetString(0);
+            }
+            Console.WriteLine(contactName);
+        }
+        public static void GetFullCustomerAddress(int customerID)
+        {
+            string FullCustomerAddress = "not found";
+            using var connection = new MySqlConnection(connStr);
+            connection.Open();
+            using var command = new MySqlCommand("SELECT Address, City FROM customers WHERE customerID = @customerID;", connection);
+            command.Parameters.AddWithValue("@customerID", customerID);
+            using var reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                FullCustomerAddress = $"{reader.GetString(0)}, {reader.GetString(0)}";
+            }
+            Console.WriteLine(FullCustomerAddress);
         }
     }
 }
